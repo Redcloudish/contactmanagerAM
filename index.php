720cbc200eb610c8e398722c4bc3bf64112af3f5
Michael Brown <160866598+Redcloudish@users.noreply.github.com>
2024-09-17 11:00:53 -0400

    getting there

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    require("database.php")
+?>
+    <!DOCTYPE html>
+<html>
+   <head>
+       <title>Contact Manager - Home</title>
+       <link rel="stylesheet" type="text/css" href="css/main.css" />       
+   </head>
+   <body>
+       <?php include("header.php"); ?>
+       <main>
+
+       </main>
+       <?php include("footer.php"); ?>
+   </body>
+</html>
